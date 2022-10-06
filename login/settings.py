f6bdb3f8a7b93dcce4cdfb30456a87b42fc93f37
tarang187 <2020.tarang.rajpal@ves.ac.in>
2022-10-06 09:40:39 +0530

    proto1

--- login/settings.py
+++ login/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z6y1o0$i$der*d@rmj30$!ox2_spr!eqk^_drsc)&*&x^n*^2%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['loginprototype.herokuapp.com', '127.0.0.1', '.herokuapp.com']
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -128,3 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #added manually
 
+STATICFILES_DIRS = ( 
+    os.path.join('static'), 
+)
